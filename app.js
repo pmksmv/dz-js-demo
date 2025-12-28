08b404ea31cd0701f831b06ea161b9f45c4a282a
pmksmv <maksimov1207@gmail.com>
2025-12-28 15:19:51 +0300

    Исправлена опечатка в названии переменны address

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // Исходные данные
-const adressLat = 100;
-const adressLong = 200;
+const addressLat = 100;
+const addressLong = 200;
 const positionLat = 10;
 const positionLong = 20;
 
 //Расчет расстояния
 
-let distanceLat = adressLat - positionLat;
-let distanceLong = adressLong - positionLong;
+let distanceLat = addressLat - positionLat;
+let distanceLong = addressLong - positionLong;
 let _distance = (distanceLat ** 2 + distanceLong ** 2) ** 0.5;
 
 console.log(`Расстояние до точки назначения равно: ${_distance.toFixed(2)}`);
